796baeba9fccfac9ed56681f5609ec6ef1ca4019
Alexandr Chebatarev <alexandr.chebatarev@gmail.com>
2025-06-18 11:39:31 -0400

    Add tests + handle edge cases

--- app/lib/normalMode.ts
+++ app/lib/normalMode.ts
@@ -1,40 +1,93 @@
 import { Mode, State } from "./utils";
 
+enum Direction {
+  RIGHT,
+  LEFT,
+}
+
+function findNewLine(
+  text: string,
+  location: number,
+  direction: Direction,
+): number {
+  if (text[location] == "\n") {
+    return location;
+  }
+
+  if (direction == Direction.LEFT) {
+    for (let i = location - 1; i >= 0; i--) {
+      if (text[i] == "\n") return i;
+    }
+  } else if (direction == Direction.RIGHT) {
+    for (let i = location + 1; i <= text.length; i++) {
+      if (text[i] == "\n") return i;
+    }
+  }
+  return -1;
+}
+
 export function handle(event: KeyboardEvent, state: State): State {
+  console.log(event);
   const newState = { ...state };
-  console.log(state, event);
+  const rightNewLine = findNewLine(state.text, state.location, Direction.RIGHT);
+  const leftNewLine = findNewLine(state.text, state.location, Direction.LEFT);
+  const onNewLine =
+    rightNewLine == state.location && leftNewLine == state.location;
   switch (event.key) {
     case "h": {
-      newState.location = Math.max(state.location - 1, -1);
-      break;
-    }
-    case "j": {
-      // Scan forward until next \n, and when found put location there.
-      // For now I would support only jumping to the beginning of the line
-      for (let i = state.location + 1; i < state.text.length; i++) {
-        if (state.text[i] == "\n") {
-          newState.location = i;
-          break;
-        }
-      }
-      break;
-    }
-    case "k": {
-      if (state.text[state.location] == "\n") {
-        newState.location = Math.max(state.location - 1, -1);
-        break;
-      }
-      for (let i = state.location - 1; i >= 0; i--) {
-        console.log(state, i);
-        if (state.text[i] == "\n") {
-          newState.location = Math.max(i - 1, -1);
-          break;
-        }
-      }
+      if (onNewLine) break;
+      newState.location = Math.max(
+        state.location - 1,
+        leftNewLine + 1, // +1 is to cover the case when I'm already on a new line
+        -1, // I don't need this one because findNewLine covers it, but I'll keep it just in case, function implementation changes
+      );
       break;
     }
     case "l": {
-      newState.location = Math.min(state.location + 1, state.text.length - 1);
+      if (onNewLine) break;
+      newState.location = Math.min(
+        state.location + 1,
+        rightNewLine == -1 ? state.text.length - 1 : rightNewLine - 1,
+      );
+      break;
+    }
+    //Allowed to hit \n
+    case "j": {
+      if (rightNewLine == -1) break; // can't go down
+      if (onNewLine) {
+        if (state.text[state.location + 2] == "\n") {
+          newState.location += 1;
+        } else {
+          newState.location += 2;
+        }
+        break;
+      }
+      if (rightNewLine == state.location) {
+        newState.location = rightNewLine + 1;
+        break;
+      }
+      if (
+        state.text[rightNewLine + 1] == "\n" ||
+        state.text[rightNewLine + 1] == undefined
+      ) {
+        newState.location = rightNewLine;
+        break;
+      }
+      newState.location = rightNewLine + 1;
+      break;
+    }
+    //Allowed to hit \n
+    case "k": {
+      if (leftNewLine == -1) break;
+      if (onNewLine) {
+        newState.location -= 1;
+        break;
+      }
+      if (leftNewLine == 0) {
+        newState.location = leftNewLine;
+      } else if (leftNewLine != -1) {
+        newState.location = leftNewLine - 1;
+      }
       break;
     }
     case "i": {
